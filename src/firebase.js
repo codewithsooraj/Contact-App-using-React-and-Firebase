723a809ca2d5bd5df35364216ff9618c4e94f293
Sooraj <codewithsooraj@gmail.com>
2022-01-17 11:31:27 +0530

    changes in firebase and addedit files

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import initializeApp from "firebase/app";
-import "firebase/database"
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCVFWzISrEc2SqwzNag7acMwbrdsRNc8uw",
-    authDomain: "contact-app-67103.firebaseapp.com",
-    projectId: "contact-app-67103",
-    storageBucket: "contact-app-67103.appspot.com",
-    messagingSenderId: "41796450697",
-    appId: "1:41796450697:web:1e16cd7918fa80f57ec958"
+  apiKey: "AIzaSyCVFWzISrEc2SqwzNag7acMwbrdsRNc8uw",
+  authDomain: "contact-app-67103.firebaseapp.com",
+  projectId: "contact-app-67103",
+  storageBucket: "contact-app-67103.appspot.com",
+  messagingSenderId: "41796450697",
+  appId: "1:41796450697:web:1e16cd7918fa80f57ec958",
 };
 
-
-const app = initializeApp(firebaseConfig);
-export default app.database().ref();
+const appDb = firebase.initializeApp(firebaseConfig);
+export default appDb.database().ref();
--- src/pages/AddEdit.js
+++ src/pages/AddEdit.js
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import "./AddEdit.css";
+import appDb from "../firebase";
+import { toast } from "react-toastify";
 
+const initialState = {
+  name: "",
+  email: "",
+  contact: "",
+};
 const AddEdit = () => {
+  const [state, setState] = useState(initialState);
+  const [data, setData] = useState({});
+  const { name, email, contact } = state;
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !email || !contact) {
+      toast.error("Please provide value in each input feild");
+    } else {
+      appDb.child("contacts").push(state, (err) => {
+        if (err) {
+          toast.error(err);
+        } else {
+          toast.success("Contact Added Successfully" );
+        }
+      });
+      setTimeout(() => navigate("/"), 800);
+    }
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
   return (
-    <div>
-      <h2>Add Edit</h2>
+    <div className="formArea">
+      <form className="formData" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter Name"
+          value={name}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Email Address"
+          value={email}
+          onChange={handleInputChange}
+        />
+
+        <label htmlFor="contact">Contact</label>
+        <input
+          type="number"
+          id="contact"
+          name="contact"
+          placeholder="Enter Contact No."
+          value={contact}
+          onChange={handleInputChange}
+        />
+
+        <input type="submit" value="Save" />
+      </form>
     </div>
   );
 };
